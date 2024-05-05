52b285a74bba528006598427f5d559459d452765
he-who-must-not-be-nam3d <153522610+he-who-must-not-be-nam3d@users.noreply.github.com>
2024-05-05 17:35:48 +0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const api_key = "e01b0537996f6351c6ec7e31980bd437";
+const api_key = "YOUR_TMDB_API_KEY";
 const imageBaseURL = "http://image.tmdb.org/t/p/";
 
 // Fetch data from server using 'url' and pass the result in JSON data to a 'callback' function along with optional parameter if has 'optionalParam'
